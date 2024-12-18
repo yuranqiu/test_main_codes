a2fe6f0097e9290426eb9413f665b046e49ca63e
yueruiqing2003 <yueruiqing.ncut@vip.163.com>
2024-12-18 13:30:43 +0800

    01.自己写一个APP组件

---
+++ hello_vue3/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    <h1>你好</h1>
+  </div>
+
+
+
+</template>
+
+<script>
+  export default{
+    name:'App'
+  }
+
+</script>
+
+<style>
+  .app{
+    background-color: aliceblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+
+
+</style>
