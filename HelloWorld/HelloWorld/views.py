6b8e46a63d1e975514070abf296da72f7ff53076
雨染秋 <yueruiqing.ncut@vip.163.com>
2024-12-18 08:36:58 +0800

    Delete HelloWorld directory

--- HelloWorld/HelloWorld/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-def runoob(request):
-    views_dict = {"name":"菜鸟教程"}
-    return render(request, "runoob.html", {"views_dict": views_dict})
-
-def hello(request):
-    return HttpResponse("Hello world ! ")
