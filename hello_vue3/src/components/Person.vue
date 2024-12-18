3b4e992fac4e4b4d4f6a7e77e11287d3d68afa71
yueruiqing2003 <yueruiqing.ncut@vip.163.com>
2024-12-18 15:34:52 +0800

    08.reactive创建对象类型的响应式数据

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,37 +1,36 @@
 <template>
     <div class="person">
-      <h2>姓名:{{ name }}</h2>
-      <h2>年龄:{{ age }}</h2>
-      <h2>地址:{{ address }}</h2>
-      <button @click="changeAge">修改年龄</button>
-      <button @click="changeName">修改姓名</button>
-      <button @click="showTel">查看联系方式</button>
+        <h2>一辆{{ car.brand }}, 价值{{ car.price }}万</h2>
+        <button @click="changePrice">修改汽车的价格</button>
+        <ul>
+            <li v-for="g in games" :keys="g.id">{{ g.name }}</li>
+        </ul>
+        <button @click="changeFirstGame">修改第一个游戏的名字</button>
     </div>
   
-  </template>
+</template>
   
-    <script lang='ts' setup name ="Person">
-    import {ref} from 'vue'
+<script lang='ts' setup name ="Person">
+    import { reactive } from 'vue';
 
     //数据
-    let name = ref('张三')
-    let age = ref(18)
-    let tel = '1564156156'
-    let address = '北京市'
-
+    let car = reactive({brand:'奔驰', price:100})
+    let games = reactive([
+        {id:'sadasa1',name:'1111'},
+        {id:'sasass2',name:'2222'},
+        {id:'sasas3s',name:'3333'}
+    ])
     //方法
-    function changeName() {
-        name.value = 'zhang-san'
+    function changePrice(){
+        car.price += 10
     }
-    function changeAge() {
-        age.value += 1
+    function changeFirstGame(){
+        games[0].name = '6666'
+
     }
-    function showTel() {
-        alert(tel)
-    }
-  </script>
+</script>
   
-  <style>
+<style scoped>
     .person{
       background-color: aliceblue;
       box-shadow: 0 0 10px;
@@ -42,4 +41,4 @@
         margin: 0 10px;
     }
    
-  </style>
+</style>
