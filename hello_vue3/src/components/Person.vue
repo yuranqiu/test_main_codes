5f5275fbb8644b23210c4150385bc403cac32aa1
yueruiqing2003 <yueruiqing.ncut@vip.163.com>
2024-12-19 10:56:56 +0800

    10.computed计算属性

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,32 +1,38 @@
 <template>
     <div class="person">
-        <h2>一辆{{ car.brand }}, 价值{{ car.price }}万</h2>
-        <button @click="changePrice">修改汽车的价格</button>
-        <ul>
-            <li v-for="g in games" :keys="g.id">{{ g.name }}</li>
-        </ul>
-        <button @click="changeFirstGame">修改第一个游戏的名字</button>
+    姓: <input type="text" v-model="firstName"> <br>
+    名: <input type="text" v-model="lastName"> <br>
+    全名: <span>{{fullName }}</span>
+    <button @click="changeFullName">将全名修改为li -si</button>
     </div>
-  
 </template>
   
 <script lang='ts' setup name ="Person">
-    import { reactive } from 'vue';
+    import {ref,computed} from 'vue'
 
-    //数据
-    let car = reactive({brand:'奔驰', price:100})
-    let games = reactive([
-        {id:'sadasa1',name:'1111'},
-        {id:'sasass2',name:'2222'},
-        {id:'sasas3s',name:'3333'}
-    ])
-    //方法
-    function changePrice(){
-        car.price += 10
-    }
-    function changeFirstGame(){
-        games[0].name = '6666'
+    let firstName = ref('张')
+    let lastName = ref('三')
 
+    //此计算属性无法修改，是只读的
+    // let fullName = computed(()=>{
+    //     return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+    // })
+
+    //如此的计算属性是可以进行修改的，可读可写的。
+    let fullName = computed({
+       get(){
+        return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value 
+       },
+       set(val){
+        const [str1,str2] = val.split('-')
+        firstName.value = str1
+        lastName.value = str2
+       }
+        
+    })
+
+    function changeFullName(){
+        fullName.value ='li-si'
     }
 </script>
   
