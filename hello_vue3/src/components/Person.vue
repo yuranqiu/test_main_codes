6be1c965b738171a15664439aab5df0a3e5eced0
yueruiqing2003 <yueruiqing.ncut@vip.163.com>
2024-12-20 13:41:21 +0800

    13.watch监视情况三

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="person">
-    <h1>情况二:监视ref定义的【对象】数据</h1>
+    <h1>情况三:监视【reactive】定义的【对象】数据</h1>
     <h2>姓名：{{ person.name }}</h2>
     <h2>年龄:{{ person.age }}</h2>
     <button @click="changeName">修改名字</button>
@@ -10,24 +10,24 @@
 </template>
   
 <script lang='ts' setup name ="Person">
-    import {ref,watch} from 'vue'
+    import {reactive,watch} from 'vue'
     //数据
-    let person = ref({
+    let person = reactive({
         name:"zhangsan",
         age:18
     })
     //方法
     function changeName(){
-        person.value.name += '~'
+        person.name += '~'
     }
     function changeAge(){
-        person.value.age += 1
+        person.age += 1
     }
     function changePerson(){
-        person.value = {name:'李四', age:90}
+       Object.assign(person,{name:'李四', age:90})
     }
     
-    //监视,情况一:监视ref定义的【对象类型】数据，监视的是对象的地址值，若想监视对象内部属性的变化，需要手动开启深度监视
+    //监视,情况三:监视reactive定义的【对象类型】数据，且默认是开启深度监视的
     //watch的第一个参数:被监视的数据，第二个参数:监视的回调,第三个参数:配置对象
     watch(person,(newValue,oldValue)=>{
         console.log('person变化了',newValue,oldValue)
