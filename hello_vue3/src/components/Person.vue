c6006211f7bf45f83f2ce7930e5e27f68c46b8db
yueruiqing2003 <yueruiqing.ncut@vip.163.com>
2024-12-20 09:56:04 +0800

    11.watch监视情况一

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,39 +1,26 @@
 <template>
     <div class="person">
-    姓: <input type="text" v-model="firstName"> <br>
-    名: <input type="text" v-model="lastName"> <br>
-    全名: <span>{{fullName }}</span>
-    <button @click="changeFullName">将全名修改为li -si</button>
+    <h1>情况一:监视ref定义的基本类型数据</h1>
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="changeSum">点我sum+1</button>
     </div>
 </template>
   
 <script lang='ts' setup name ="Person">
-    import {ref,computed} from 'vue'
-
-    let firstName = ref('张')
-    let lastName = ref('三')
-
-    //此计算属性无法修改，是只读的
-    // let fullName = computed(()=>{
-    //     return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
-    // })
-
-    //如此的计算属性是可以进行修改的，可读可写的。
-    let fullName = computed({
-       get(){
-        return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value 
-       },
-       set(val){
-        const [str1,str2] = val.split('-')
-        firstName.value = str1
-        lastName.value = str2
-       }
-        
-    })
-
-    function changeFullName(){
-        fullName.value ='li-si'
+    import {ref,watch} from 'vue'
+    //数据
+    let sum = ref(0)
+    //方法
+    function changeSum(){
+        sum.value += 1
     }
+    //监视，情况一：监视[ref]定义的[基本类型]数据
+    const stopWatch = watch(sum,(newValue,oldValue)=>{
+        console.log('sum变化了',newValue,oldValue)
+        if (newValue>=10) {
+            stopWatch()
+        }
+    })
 </script>
   
 <style scoped>
