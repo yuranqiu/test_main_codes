efbd0548cf6e694b139040ba86fb144181e526e8
yueruiqing2003 <yueruiqing.ncut@vip.163.com>
2024-12-20 17:24:08 +0800

    18.props的使用

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,32 +1,46 @@
 <template>
     <div class="person">
-
+      <ul>
+        <li v-for="p in list" :key="p.id">
+          {{p.name}} -- {{p.age}}
+        </li>
+      </ul>
     </div>
-</template>
+  </template>
   
-<script lang='ts' setup name ="Person">
-    import {ref} from 'vue'
-    import { type PersonInter,type Persons } from '@/types';
-
-
-    let person:PersonInter = {id:'232321323',name:'张三',age:60}
-
-    let PersonList:Persons = [
-        {id:'232321323',name:'张三',age:60},
-        {id:'232321323',name:'张三',age:60},
-        {id:'232321323',name:'张三',age:60}
-    ]
-</script>
+  <script lang="ts" setup name="Person">
+    import {withDefaults} from 'vue'
+    import {type Persons} from '@/types'
   
-<style>
-    .person{
-      background-color: aliceblue;
+    // 只接收list
+    // defineProps(['list'])
+  
+    // 接收list + 限制类型
+    // defineProps<{list:Persons}>()
+  
+    //  接收list + 限制类型 + 限制必要性 + 指定默认值
+    withDefaults(defineProps<{list?:Persons}>(),{
+      list:()=> [{id:'ausydgyu01',name:'康师傅·王麻子·特仑苏',age:19}]
+    })
+  
+  
+    // 接收list，同时将props保存起来
+    /* let x = defineProps(['list'])
+    console.log(x.list) */
+  
+  </script>
+  
+  <style scoped>
+    .person {
+      background-color: skyblue;
       box-shadow: 0 0 10px;
       border-radius: 10px;
       padding: 20px;
     }
-    button{
-        margin: 0 10px;
+    button {
+      margin: 0 5px;
     }
-   
-</style>
+    li {
+      font-size: 20px;
+    }
+  </style>
