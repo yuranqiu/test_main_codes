c93e59d0f757fa983a7717e63a2c2d3d79613342
yueruiqing2003 <yueruiqing.ncut@vip.163.com>
2024-12-20 14:09:36 +0800

    14.watch监视情况四

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -3,9 +3,12 @@
     <h1>情况三:监视【reactive】定义的【对象】数据</h1>
     <h2>姓名：{{ person.name }}</h2>
     <h2>年龄:{{ person.age }}</h2>
+    <h2>汽车:{{ person.car.c1}}、{{ person.car.c2 }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
-    <button @click="changePerson">修改对象</button>
+    <button @click="changeC1">修改第一台车</button>
+    <button @click="changeC2">修改第二台车</button>
+    <button @click="changeCar">修改整个车</button>
     </div>
 </template>
   
@@ -13,8 +16,12 @@
     import {reactive,watch} from 'vue'
     //数据
     let person = reactive({
-        name:"zhangsan",
-        age:18
+        name:"张三",
+        age:18,
+        car:{
+            c1:'bench',
+            c2:'bmw'
+        }
     })
     //方法
     function changeName(){
@@ -23,15 +30,21 @@
     function changeAge(){
         person.age += 1
     }
-    function changePerson(){
-       Object.assign(person,{name:'李四', age:90})
+    function changeC1(){
+       person.car.c1 = 'aodi'
+    }
+    function changeC2(){
+       person.car.c2 = 'auto'
+    }
+    function changeCar(){
+        person.car = {c1:'雅迪',c2:'艾玛'}
     }
     
-    //监视,情况三:监视reactive定义的【对象类型】数据，且默认是开启深度监视的
+    //监视,情况四:监视响应式对象中的某个属性，且该属性是基本类型，要写成函数式
     //watch的第一个参数:被监视的数据，第二个参数:监视的回调,第三个参数:配置对象
-    watch(person,(newValue,oldValue)=>{
-        console.log('person变化了',newValue,oldValue)
-    },{deep:true,immediate:true})
+    watch(()=>person.car,(newValue,oldValue)=>{
+        console.log('person.car变化了',newValue,oldValue)
+    },{deep:true})
     
 </script>
   
